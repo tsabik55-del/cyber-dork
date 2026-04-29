67bf10bb617d9434ae023c3a48d57b0faa80007a
riyu <tsabik55@gmail.com>
2026-04-29 11:26:10 +0700

    Add sitemap function to generate site map data

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://cyber-dork.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
